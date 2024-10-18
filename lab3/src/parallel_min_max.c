440383219a60d66eab46b430a698d49b1c1e0589
pohotlivyiborov <andreyhasnas2003@gmail.com>
2024-10-18 21:59:08 +0000

    Изменён файл parallel_min_max.c

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -15,6 +15,22 @@
 #include "find_min_max.h"
 #include "utils.h"
 
+int timeout = 0; //таймаут по умолчанию
+
+
+void kill_children(pid_t *child_pids, int pnum) {
+    for (int i = 0; i < pnum; i++) {
+        if (child_pids[i] > 0) {
+            kill(child_pids[i], SIGKILL);
+        }
+    }
+}
+
+void timeout_handler(int signum) {
+    printf("Timeout reached, killing child processes...\n");
+    exit(1);
+}
+
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
@@ -40,18 +56,21 @@ int main(int argc, char **argv) {
         switch (option_index) {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            if (seed <=0){
+              printf("Seed must be a positive integer\n");
+            }
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            if(array_size <= 0){
+              printf("Array size must be a positive integer\n");
+            }
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            if(pnum <= 0){
+              printf("Number of processes must be a positive integer\n");
+            }
             break;
           case 3:
             with_files = true;
@@ -91,6 +110,20 @@ int main(int argc, char **argv) {
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
+
+  int pipefd[2 * pnum]; // pipe array
+
+  if (!with_files){
+    for(int i = 0; i < pnum; i++){
+      if (pipe(pipefd + i*2) < 0){
+        perror("pipe");
+        return 1;
+      }
+    }
+  }
+
+  pid_t child_pids[pnum];
+
   for (int i = 0; i < pnum; i++) {
     pid_t child_pid = fork();
     if (child_pid >= 0) {
@@ -98,15 +131,30 @@ int main(int argc, char **argv) {
       active_child_processes += 1;
       if (child_pid == 0) {
         // child process
-
-        // parallel somehow
+        int chunk_size = array_size / pnum;
+        int start_index = i * chunk_size;
+        int end_index = (i == pnum - 1) ? array_size : start_index + chunk_size;
+        
+        struct MinMax min_max = GetMinMax(array, start_index, end_index);
 
         if (with_files) {
           // use files here
+          char filename[256];
+          sprintf(filename, "result_%d.txt", i);
+          FILE *file = fopen(filename, "w");
+          fprintf(file, "%d %d\n", min_max.min, min_max.max);
+          fclose(file);
         } else {
           // use pipe here
+          close(pipefd[i*2]); // закрыть канал для чтения
+          write(pipefd[i*2 + 1], &min_max, sizeof(struct MinMax));
+          close(pipefd[i*2 + 1]); // закрыть канал для записи
         }
-        return 0;
+
+        free(array);
+        exit(0);
+      } else{
+        child_pids[i]= child_pid;
       }
 
     } else {
@@ -116,9 +164,15 @@ int main(int argc, char **argv) {
   }
 
   while (active_child_processes > 0) {
-    // your code here
+    int status;
+    pid_t pid = wait(&status);
+    if (pid > 0){
+      active_child_processes -= 1;
+    }
+  }
 
-    active_child_processes -= 1;
+  if (timeout > 0){
+    ualarm(0, 0); // отключаем таймер, если все дочерние процессы завершились до истечения таймаута
   }
 
   struct MinMax min_max;
@@ -131,12 +185,25 @@ int main(int argc, char **argv) {
 
     if (with_files) {
       // read from files
+      char filename[256];
+      sprintf(filename, "result_%d.txt", i);
+      FILE *file = fopen(filename, "r");
+      int min, max;
+      fscanf(file, "%d %d", &min, &max);
+      fclose(file);
+
+      if (min < min_max.min) min_max.min = min;
+      if (max > min_max.max) min_max.max = max;
     } else {
       // read from pipes
-    }
+      struct MinMax child_min_max;
+      close(pipefd[i*2 +1]); // закрыть канал для записи
+      read(pipefd[i*2], &child_min_max, sizeof(struct MinMax));
+      close(pipefd[i*2]); //закрыть канал для чтения
 
-    if (min < min_max.min) min_max.min = min;
-    if (max > min_max.max) min_max.max = max;
+      if (min < min_max.min) min_max.min = min;
+      if (max > min_max.max) min_max.max = max;
+    }
   }
 
   struct timeval finish_time;
